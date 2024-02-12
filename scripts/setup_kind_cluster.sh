427ec897e57d4a3cc21191d342f3a572e33313cb
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-02-12 13:47:41 +0100

    feat: added the kind installation according to the system architecture

--- scripts/setup_kind_cluster.sh
+++ scripts/setup_kind_cluster.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Check system architecture
+ARCH=$(uname -m)
+
 # Check if docker is installed on the system if it isn't then install it
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Installing Docker..."
@@ -19,8 +22,21 @@ else
     echo "Docker is already installed."
 fi
 
+# Install kind depending on the system architecture
+if [ $ARCH = "x86_64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64"
+elif [ $ARCH = "aarch64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
 
-# # Install kind depending on the system architecture
-# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
-# chmod +x ./kind
-# mv ./kind /usr/local/bin/kind
+echo "Downloading kind binary for $ARCH..."
+curl -Lo ./kind $KIND_URL
+chmod +x ./kind
+
+# Move kind binary to /usr/local/bin
+sudo mv ./kind /usr/local/bin/kind
+
+echo "kind has been installed."
