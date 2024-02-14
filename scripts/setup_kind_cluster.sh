040045bb5ef234e302646ae611e4057bd33cd4f9
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-02-14 04:17:18 +0100

    fix: added template files

--- scripts/setup_kind_cluster.sh
+++ scripts/setup_kind_cluster.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-# # Check system architecture
-# ARCH=$(uname -m)
+# Check system architecture
+ARCH=$(uname -m)
 
-# # Check if docker is installed on the system if it isn't then install it
-# if ! command -v docker &> /dev/null; then
-#     echo "Docker is not installed. Installing Docker..."
+# Check if docker is installed on the system if it isn't then install it
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
     
-#     # Install Docker
-#     curl -fsSL https://get.docker.com -o get-docker.sh
-#     sudo sh get-docker.sh
+    # Install Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
     
-#     # Add current user to the docker group to run docker commands without sudo
-#     sudo usermod -aG docker $USER
+    # Add current user to the docker group to run docker commands without sudo
+    sudo usermod -aG docker $USER
     
-#     # Activate the changes to groups
-#     newgrp docker
+    # Activate the changes to groups
+    newgrp docker
     
-#     echo "Docker has been installed."
-# else
-#     echo "Docker is already installed."
-# fi
+    echo "Docker has been installed."
+else
+    echo "Docker is already installed."
+fi
 
-# # Install kind depending on the system architecture
-# if [ $ARCH = "x86_64" ]; then
-#     KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64"
-# elif [ $ARCH = "aarch64" ]; then
-#     KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-arm64"
-# else
-#     echo "Unsupported architecture: $ARCH"
-#     exit 1
-# fi
+# Install kind depending on the system architecture
+if [ $ARCH = "x86_64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64"
+elif [ $ARCH = "aarch64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
 
-# echo "Downloading kind binary for $ARCH..."
-# curl -Lo ./kind $KIND_URL
-# chmod +x ./kind
+echo "Downloading kind binary for $ARCH..."
+curl -Lo ./kind $KIND_URL
+chmod +x ./kind
 
-# # Move kind binary to /usr/local/bin
-# sudo mv ./kind /usr/local/bin/kind
+# Move kind binary to /usr/local/bin
+sudo mv ./kind /usr/local/bin/kind
 
-# echo "kind has been installed."
+echo "kind has been installed."
 
 # Create a Kind cluster named "ignite-dev and enable ingress"
 cat <<EOF | kind create cluster --name ignite-dev --config=-
@@ -70,3 +70,4 @@ kind get kubeconfig --name ignite-dev > ~/.kube/config_kind
 kubectl cluster-info --context ignite-dev
 
 # Install NGINX ingress in KIND ignite-dev cluster
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,5 +15,77 @@ provider "kubectl" {
 }
 
 resource "kubectl_manifest" "node_app" {
-  yaml_body = file("${path.module}/deployment.yaml")
+  yaml_body = <<YAML
+# apiVersion: apps/v1
+# kind: Deployment
+# metadata:
+#   name: hello-world
+# spec:
+#   replicas: 1
+#   selector:
+#     matchLabels:
+#       app: hello-world
+#   template:
+#     metadata:
+#       labels:
+#         app: hello-world
+#     spec:
+#       containers:
+#         - name: hello-world
+#           image: onyekachukwu/node-hello-world
+#           ports:
+#             - containerPort: 3000
+#           resources:
+#             limits:
+#               cpu: "0.5"
+#               memory: "512Mi"
+#             requests:
+#               cpu: "0.25"
+#               memory: "256Mi"
+#           livenessProbe:
+#             httpGet:
+#               path: /
+#               port: 3000
+#             initialDelaySeconds: 15
+#             periodSeconds: 10
+#           readinessProbe:
+#             httpGet:
+#               path: /
+#               port: 3000
+#             initialDelaySeconds: 5
+#             periodSeconds: 5
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: hello-world-service
+spec:
+  selector:
+    app: hello-world
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 3000
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: hello-world-ingress
+  annotations:
+    nginx.ingress.kubernetes.io/rewrite-target: /$2
+    nginx.ingress.kubernetes.io/use-regex: "true"
+    kubernetes.io/ingress.className: "nginx"
+spec:
+  ingressClassName: nginx
+  rules:
+    - http:
+        paths:
+          - path: /(.*)
+            pathType: ImplementationSpecific
+            backend:
+              service:
+                name: hello-world-service
+                port:
+                  number: 80
+YAML
 }
